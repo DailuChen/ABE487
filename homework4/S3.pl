27e174c3f07c3e68ba58421c1c4af0de8f156a77
Ken Youens-Clark <kyclark@gmail.com>
2015-10-05 17:26:31 -0700

    suggestions

--- homework4/S3.pl
+++ homework4/S3.pl
@@ -1,34 +1,45 @@
-#!/usr/bin/perl 
+#!/usr/bin/env perl 
 
 use strict;
 use warnings;
+use autodie;
 
-my $infasta = 'Perl_III.fasta.txt';
+my $infasta  = 'Perl_III.fasta.txt';
 my $outfasta = 'Perl_III.fasta.out';
 
-open (STDIN, '<', $infasta) or die "cannot read fastain: $!\n";
-open (STDOUT,'>',$outfasta) or die "Cannot write to fastaout: $!\n";
+open my $STDIN,  '<', $infasta;
+open my $STDOUT, '>', $outfasta;
 
 my $name;
 my $sequence = '';
 
-while (my $line = <STDIN>) {
+while (my $line = <$STDIN>) {
     chomp $line;
     if ($line =~ /^>/) {
-     
-	if (length($sequence) >0) {
-	print (STDOUT $name, "_reverse_complement\n", $sequence, "\n");
-	$sequence = '';
-	}
 
-	$name = $line;
+        if (length($sequence) > 0) {
+            print($STDOUT $name, "_reverse_complement\n", $sequence, "\n");
+            $sequence = '';
+        }
+
+        $name = $line;
     }
     else {
-	$line =~ tr/atcgATCG/tagcTAGC/;
-	$sequence = reverse($line);
+        $line =~ tr/atcgATCG/tagcTAGC/;
+        $sequence = reverse($line);
     }
 }
-if (length($sequence) >0) {
-    print (STDOUT $name, "_reverse_complement\n", $sequence, "\n");
+
+if (length($sequence) > 0) {
+    print($STDOUT $name, "_reverse_complement\n", $sequence, "\n");
 }
 
+__END__
+
+use autodie!!
+
+ll. 10-11: By moving to scalars instead of global symbols, it fixed 
+all the other problems.  Never use $STDERR/$STDOUT as variable names 
+though (unless you know what you're doing).
+
+One point off.
