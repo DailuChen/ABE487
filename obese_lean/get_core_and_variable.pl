3757cd25c72c87a72b6a3a8b21e3b2a8633cafd9
dailu@email.arizona.edu <dailu@service1.ice.americas.sgi.com>
2015-11-02 17:43:51 -0700

    obeselean

---
+++ obese_lean/get_core_and_variable.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use autodie;
+use feature 'say';
+
+if (@ARGV != 4){
+    die "Please provide four files.\n";
+}
+
+my (%keggfile);
+for my $file (@ARGV) {
+    open my $fh, '<', $file;
+
+    while (my $line = <$fh>) {
+        chomp $line;
+        my ($kegg_id, $n_match) = split(/,/, $line);
+        next unless $n_match >= 50;
+        $keggfile{ $kegg_id }++;
+    }
+}
+
+my $nfiles = scalar(@ARGV);
+
+open my $core_fh, '>', 'core';
+open my $var_fh, '>', 'variable';
+
+say $core_fh join "\n",
+    grep { $keggfile{$_} == $nfiles } sort keys %keggfile;
+say $var_fh join "\n",
+    grep { $keggfile{$_} < $nfiles } sort keys %keggfile;
